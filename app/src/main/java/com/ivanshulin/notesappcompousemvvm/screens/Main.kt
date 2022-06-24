b0abeb769dfab68337a0563b67e5343799c0b4e9
IvanSonVovki <99916730+IvanSonVovki@users.noreply.github.com>
2022-06-24 07:10:41 +0300

    Lesson 3

--- app/src/main/java/com/ivanshulin/notesappcompousemvvm/screens/Main.kt
+++ app/src/main/java/com/ivanshulin/notesappcompousemvvm/screens/Main.kt
@@ -1,29 +1,42 @@
 package com.ivanshulin.notesappcompousemvvm.screens
 
+import android.app.Application
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.ivanshulin.notesappcompousemvvm.MainViewModel
+import com.ivanshulin.notesappcompousemvvm.MainViewModelFactory
+import com.ivanshulin.notesappcompousemvvm.model.Note
 import com.ivanshulin.notesappcompousemvvm.navigation.NavRoute
 import com.ivanshulin.notesappcompousemvvm.ui.theme.NotesAppCompouseMVVMTheme
 
 @Composable
 fun MainScreen(navController: NavHostController) {
 
+    val context = LocalContext.current
+    val mViewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+
+    val notes = mViewModel.readTest.observeAsState(listOf()).value
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(onClick = {
@@ -38,33 +51,40 @@ fun MainScreen(navController: NavHostController) {
         }
     ) {
 
-        Column() {
-            NoteItem(
-                title = "Note 1",
-                subtitles = "Subtitle for note 1",
-                navController = navController
-            )
-            NoteItem(
-                title = "Note 2",
-                subtitles = "Subtitle for note 2",
-                navController = navController
-            )
-            NoteItem(
-                title = "Note 3",
-                subtitles = "Subtitle for note 3",
-                navController = navController
-            )
-            NoteItem(
-                title = "Note 4",
-                subtitles = "Subtitle for note 4",
-                navController = navController
-            )
+        /*  Column() {
+              NoteItem(
+                  title = "Note 1",
+                  subtitles = "Subtitle for note 1",
+                  navController = navController
+              )
+              NoteItem(
+                  title = "Note 2",
+                  subtitles = "Subtitle for note 2",
+                  navController = navController
+              )
+              NoteItem(
+                  title = "Note 3",
+                  subtitles = "Subtitle for note 3",
+                  navController = navController
+              )
+              NoteItem(
+                  title = "Note 4",
+                  subtitles = "Subtitle for note 4",
+                  navController = navController
+              )
+          }*/
+        
+        LazyColumn {
+            items(notes) {
+                note ->
+                NoteItem(note = note, navController = navController)
+            }
         }
     }
 }
 
 @Composable
-fun NoteItem(title: String, subtitles: String, navController: NavHostController) {
+fun NoteItem(note: Note, navController: NavHostController) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -79,11 +99,11 @@ fun NoteItem(title: String, subtitles: String, navController: NavHostController)
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = title,
+                text = note.title,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
             )
-            Text(text = subtitles)
+            Text(text = note.subtitle)
         }
 
     }
--- app/src/main/java/com/ivanshulin/notesappcompousemvvm/screens/Start.kt
+++ app/src/main/java/com/ivanshulin/notesappcompousemvvm/screens/Start.kt
@@ -1,5 +1,6 @@
 package com.ivanshulin.notesappcompousemvvm.screens
 
+import android.app.Application
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
@@ -7,16 +8,25 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.ivanshulin.notesappcompousemvvm.MainViewModel
+import com.ivanshulin.notesappcompousemvvm.MainViewModelFactory
 import com.ivanshulin.notesappcompousemvvm.navigation.NavRoute
 import com.ivanshulin.notesappcompousemvvm.ui.theme.NotesAppCompouseMVVMTheme
+import com.ivanshulin.notesappcompousemvvm.utils.TYPE_FIREBASE
+import com.ivanshulin.notesappcompousemvvm.utils.TYPE_ROOM
 
 @Composable
 fun StartScreen(navController: NavHostController) {
 
+    val context = LocalContext.current
+    val mViewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
     Scaffold(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
@@ -27,6 +37,7 @@ fun StartScreen(navController: NavHostController) {
             Text(text = "What will we use?")
             Button(
                 onClick = {
+                    mViewModel.initDataBase(TYPE_ROOM)
                     navController.navigate(route = NavRoute.Main.route)
                 },
                 modifier = Modifier
@@ -38,6 +49,8 @@ fun StartScreen(navController: NavHostController) {
 
             Button(
                 onClick = {
+                    mViewModel.initDataBase(TYPE_FIREBASE)
+
                     navController.navigate(route = NavRoute.Main.route)
                 },
                 modifier = Modifier
